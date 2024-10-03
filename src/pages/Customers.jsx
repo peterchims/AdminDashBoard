c05a3358a3eba6199d639a0eca9dcbdc5af6c32e
Peter4Tech <peter4tech@gmail.com>
2024-10-03 17:01:31 +0100

    Add some functionalities to the customer page such as giving the user the ability to edit the data, sorting the data and also deleting some data

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, 
-  Inject, Edit, Sort, Filter } from '@syncfusion/ej2-react-grids';
+  Inject, Edit, Sort, Filter, 
+  Search,
+  Toolbar} from '@syncfusion/ej2-react-grids';
 
   import { customersData, customersGrid } from '../data/dummy';
 import Header from '../component/Header';
 
 const Customers = () => {
   return (
-    <div>
-      Customers 
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header category="Page" title="Customers" />
+      <GridComponent 
+      id="gridComp"
+      dataSource={customersData}
+      allowPaging
+      allowSorting
+      toolbar={['Delete']}
+      editSettings={{allowDeleting:true, allowEditing:true}}
+      width="auto"
+      >
+        <ColumnsDirective>
+        {customersGrid.map((item, index) => (
+          <ColumnDirective key={index} {...item} />
+        ))}
+        </ColumnsDirective>
+        <Inject services={[ Page,  Toolbar , Selection, Edit, Sort, Filter ]} />
+      </GridComponent>
     </div>
   );
 }
