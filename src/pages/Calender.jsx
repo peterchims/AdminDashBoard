0e5a6d8f6a5646f32877329177a7dabbb8ca866f
Peter4Tech <peter4tech@gmail.com>
2024-10-04 13:49:35 +0100

    Working on the calender page for my dashboard to give it his own functionality

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import {ScheduleComponent, ViewDirective, ViewsDirective, Day,
+  Week, WorkWeek, Month, Agenda, Inject, Resize, DragAndDrop
+} from '@syncfusion/ej2-react-schedule';
+import { DatePickerComponent } from '@syncfusion/ej2-react-calendars';
+import { scheduleData } from '../data/dummy';
+import Header from '../component/Header';
+
+
 
 const Calender = () => {
   return (
-    <div>
-      Calender
+    <div className='m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl'>
+<Header category="App" title="Calender"/>
     </div>
   )
 }
