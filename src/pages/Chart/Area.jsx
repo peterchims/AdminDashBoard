045dfbb7b155f8903a92fd1f8d17db96aea73eee
Peter4Tech <peter4tech@gmail.com>
2024-10-04 16:29:30 +0100

    Completed the dashboard page

--- src/pages/Chart/Area.jsx
+++ src/pages/Chart/Area.jsx
@@ -1,11 +1,46 @@
-import React from 'react'
+import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  SplineAreaSeries, // Include SplineAreaSeries
+  Legend
+} from '@syncfusion/ej2-react-charts';
+import Header from '../../component/Header';
+import { areaCustomSeries, areaPrimaryXAxis, areaPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContentProvider';
 
 const Area = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <div>
-      Area
+      {/* Add a header if needed */}
+      <Header category="Chart" title="Area Chart" />
+      
+      <ChartComponent
+        id="area-chart"
+        height="400px"
+        primaryXAxis={areaPrimaryXAxis} 
+        primaryYAxis={areaPrimaryYAxis}  
+        chartArea={{ border: { width: 0 } }}  
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'}  
+        tooltip={{ enable: true }} 
+      >
+        {/* Inject services as an array */}
+        <Inject services={[SplineAreaSeries, DateTime, Legend]} />
+        
+        {/* Series Collection */}
+        <SeriesCollectionDirective>
+          {areaCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} />  
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Area
+export default Area;
--- src/pages/Chart/Bar.jsx
+++ src/pages/Chart/Bar.jsx
@@ -1,11 +1,49 @@
-import React from 'react'
+import React from 'react';
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  BarSeries, // Use BarSeries instead of SplineAreaSeries
+  Legend,
+  Category,  // Add Category for categorical X axis if needed
+  Tooltip
+} from '@syncfusion/ej2-react-charts';
+import Header from '../../component/Header';
+import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+import { useStateContext } from '../../contexts/ContentProvider';
 
 const Bar = () => {
+  const { currentMode } = useStateContext();
+
   return (
     <div>
-      Bar Chart
+      {/* Header for the chart */}
+      <Header category="Chart" title="Bar Chart" />
+      
+      <ChartComponent
+        id="bar-chart"
+        height="400px"
+        primaryXAxis={barPrimaryXAxis}  
+        primaryYAxis={barPrimaryYAxis}  
+        chartArea={{ border: { width: 0 } }}  
+        background={currentMode === 'Dark' ? '#33373E' : '#fff'} 
+        tooltip={{ enable: true }}  
+      >
+        {/* Inject necessary services for the Bar chart */}
+        <Inject services={[BarSeries, DateTime, Legend, Tooltip, Category]} />
+        
+        {/* Series Collection for Bar chart */}
+        <SeriesCollectionDirective>
+          {barCustomSeries.map((item, index) => (
+            <SeriesDirective key={index} {...item} /> 
+          ))}
+        </SeriesCollectionDirective>
+      </ChartComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Bar
+export default Bar;
+
